149f0a0edc82295bb246776aef6b12229588fc84
Aniket Ajit Andhale <122631373+aniketandhale123@users.noreply.github.com>
2023-11-07 10:22:18 +0530

    Add files via upload

---
+++ example40.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+
+
+int getlength(char name[])
+{
+    int count =0 ;
+    for (int i=0 ; name[i] != '\0' ; i++)
+    {
+        count ++ ;
+
+    }
+return count ;
+}
+
+int main ()
+{
+    char name[20];
+    cout<<"enter your name"<<endl;
+    cin>>name;
+ 
+
+    int length = getlength(name );
+
+    cout<<"length of string is "<<length<<endl;
+
+
+}
